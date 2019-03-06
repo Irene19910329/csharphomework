aeeba8fadf3e750514c8df55752c2bc486ad4498
Wu Peihan <496095754@qq.com>
2019-03-06 19:30:37 +0800

    hmw2

---
+++ homework2/hw2.6/hw2.6/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace hw2._6
+{
+    class Program
+    {
+        static void judge_prime(int number)
+        {
+            int count=0;
+            for(int i=1;i<=number;i++)
+            {
+                if (number % i == 0)
+                    count++;
+            }
+            if (count == 2)
+                Console.WriteLine(number);
+        }
+
+        static void Main(string[] args)
+        {
+            int a;
+            Console.WriteLine("请输入一个数");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("所有素数因子为:");
+            for(int i=1;i<=a;i++)
+            {
+                if (a % i == 0)
+                    judge_prime(i);
+            }
+        }
+    }
+}
---
+++ homework2/hw2.9/hw2.9/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace hw2._9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = new int[99];
+
+            for (int i1 = 0; i1 < 99; i1++)
+            {
+                a[i1] = i1 + 2;
+            }//给数组赋值2——100
+
+            for (int i = 2; i <= 100; i++)
+            {
+                for (int j = 0; j < 99; j++)
+                {
+                    if (a[j] != 0)
+                    {
+                        if (a[j] % i == 0 && a[j] / i != 1)
+                        { a[j] = 0; }
+                    }
+                }
+            }//筛选出素数，数组素数为本身，非素数为0
+
+            Console.WriteLine("输出2——100之间的所有素数");
+            int cout = 0;
+            for (int ii = 0; ii < 99; ii++)
+            {
+                if (a[ii] != 0)
+                {
+                    Console.Write(a[ii] + " ");
+                    cout++;
+                    if (cout % 10 == 0)
+                    { Console.WriteLine(); }
+                }
+            }//输出素数每行10个
+        }
+
+    }
+        }
+    
